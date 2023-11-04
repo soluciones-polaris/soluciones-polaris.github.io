4db324b9dcf468ec887583c47d4eb04f1e673c91
soluciones-polaris <mb0whoami@gmail.com>
2023-11-04 16:07:56 -0500

    update

--- cargador.js
+++ cargador.js
@@ -9,3 +9,19 @@ function cargarBanner()
   .then(x => x.text())
   .then(y => document.getElementById("banner").innerHTML = y);
 }
+
+function cargarMenu()
+{
+  fetch ("menu.html")
+  .then(x => x.text())
+  .then(y => document.getElementById("menu").innerHTML = y);
+}
+
+function updatemenu() {
+  if (document.getElementById('responsive-menu').checked == true) {
+    document.getElementById('menu').style.borderBottomRightRadius = '0';
+    document.getElementById('menu').style.borderBottomLeftRadius = '0';
+  }else{
+    document.getElementById('menu').style.borderRadius = '3px';
+  }
+}
