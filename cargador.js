73275fad5575a8d7cfc0376ea67a2f5a3b0e3eda
soluciones-polaris <mb0whoami@gmail.com>
2023-11-04 15:01:33 -0500

    update

--- cargador.js
+++ cargador.js
@@ -1,3 +1,11 @@
-fetch ("banner.html")
-.then(x => x.text())
-.then(y => document.getElementById("banner").innerHTML = y);
+document.addEventListener("DOMContentLoaded",()=>{
+  cargarBanner()
+
+})
+
+function cargarBanner()
+{
+  fetch ("banner.html")
+  .then(x => x.text())
+  .then(y => document.getElementById("banner").innerHTML = y);
+}
