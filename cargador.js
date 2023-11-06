ecfdfa8b53099af6da449714582fcb26f432f7e6
soluciones-polaris <mb0whoami@gmail.com>
2023-11-06 12:01:16 -0500

    update

--- cargador.js
+++ cargador.js
@@ -2,9 +2,27 @@ document.addEventListener("DOMContentLoaded",()=>{
   cargarBanner()
   cargarMenu()
   cargarMenuH()
+  pintarMenu()
 
 })
+function pintarMenu()
+{
+  let btnMenu=document.querySelectorAll("#menu > div > a")
+  
+  if(location.href.search(/.*todoelectro.*/ig) != -1)
+  document.querySelectorAll("#menu > div > a")[1].style.backgroundColor="#ccc"
+  else if(location.href.search(/.*mantenimiento.*/ig) != -1)
+  document.querySelectorAll("#menu > div > a")[2].style.backgroundColor="#ccc"
+  else if(location.href.search(/.*pcb.*/ig) != -1)
+    document.querySelectorAll("#menu > div > a")[3].style.backgroundColor="#ccc"
+  else if(location.href.search(/.*contacto.*/ig) != -1)
+    document.querySelectorAll("#menu > div > a")[4].style.backgroundColor="#ccc"
+  else if(location.href.search(/.*portafolio.*/ig) != -1)
+    document.querySelectorAll("#menu > div > a")[5].style.backgroundColor="#ccc"
+  else 
+  document.querySelectorAll("#menu > div > a")[0].style.backgroundColor="#ccc"
 
+}
 function cargarBanner()
 {
   fetch ("banner.html")
