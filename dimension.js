d1a62dab47d333694bd5e3c50c34ad8093c88b91
soluciones-polaris <mb0whoami@gmail.com>
2023-12-23 12:32:25 -0500

    subiendo archivos

---
+++ dimension.js
@@ -0,0 +1,11 @@
+window.addEventListener('resize', function() {
+  // Obtener el ancho y alto de la ventana
+  let anchoVentana = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  let altoVentana = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  if(anchoVentana>=1249)
+  {
+    w3_close()
+  }
+  // Código que se ejecutará cuando la ventana se redimensione
+  // console.log('La ventana se ha redimensionado. Ancho:', anchoVentana, 'Alto:', altoVentana);
+});
