9f4c1650af954de47997d3e3cb95dfd66dd9d87b
soluciones-polaris <mb0whoami@gmail.com>
2023-12-27 23:14:50 -0500

    subiendo archivos

--- tab-gallery.js
+++ tab-gallery.js
@@ -3,5 +3,6 @@ function myFunction(imgs) {
   let imgText = document.getElementById("imgtext");
   expandImg.src = imgs.src;
   imgText.innerHTML = imgs.alt;
+  imgText.style="text-shadow: 4px 2px 2px rgba(0, 0, 0, 1);"
   expandImg.parentElement.style.display = "block";
 }
