8c06c039b0c0929546be7edd6868a16978c14aa9
soluciones-polaris <mb0whoami@gmail.com>
2023-12-28 10:40:46 -0500

    subiendo archivos

--- dimension.js
+++ dimension.js
@@ -1,11 +1,17 @@
-window.addEventListener('resize', function() {
-  // Obtener el ancho y alto de la ventana
-  let anchoVentana = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-  let altoVentana = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-  if(anchoVentana>=1249)
+window.addEventListener('load', function ()
+{
+  window.addEventListener('resize', function() 
   {
-    w3_close()
-  }
-  // Código que se ejecutará cuando la ventana se redimensione
-  // console.log('La ventana se ha redimensionado. Ancho:', anchoVentana, 'Alto:', altoVentana);
+    // Obtener el ancho y alto de la ventana
+    let anchoVentana = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    let altoVentana = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    if(anchoVentana>=1249)
+    {
+      w3_close()
+    }
+    render_altura()
+    
+    // Código que se ejecutará cuando la ventana se redimensione
+    // console.log('La ventana se ha redimensionado. Ancho:', anchoVentana, 'Alto:', altoVentana);
+  });
 });
