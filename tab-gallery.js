42adee1b08bcffc920f757c59b4816c5ec2a4dff
soluciones-polaris <mb0whoami@gmail.com>
2023-12-28 00:01:44 -0500

    subiendo archivos

--- tab-gallery.js
+++ tab-gallery.js
@@ -1,14 +1,16 @@
-function myFunction(imgs) {
+function myFunction(imgs)
+{
   let expandImg = document.getElementById("expandedImg");
   let imgText = document.getElementById("imgtext");
   expandImg.src = imgs.src;
   imgText.innerHTML = imgs.alt;
-  imgText.style=" color: aqua;  text-shadow: 4px 2px 2px rgba(0, 0, 0, 1);"
-  expandImg.parentElement.style.display = "block";
+  imgText.style = " color: aqua;  text-shadow: 4px 2px 2px rgba(0, 0, 0, 1);"
+    expandImg.parentElement.style.display = "block";
 }
 
-
-function setHeight(element) {
-    var nuevoAlto = element.clientWidth * 0.20;
-    element.style.height = nuevoAlto + 'px';
-  }
+function setHeight(element)
+{
+  let nuevoAlto = element.clientWidth * 0.20;
+  element.style = "height: "+nuevoAlto + 'px';
+  console.log('nuevo alto es:'+ nuevoAlto+'px')
+}
