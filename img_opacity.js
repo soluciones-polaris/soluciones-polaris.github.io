5984667ed05e573bc5aaba6309d9f316821619bc
soluciones-polaris <mb0whoami@gmail.com>
2023-12-31 17:59:28 -0500

    subiendo archivos

--- img_opacity.js
+++ img_opacity.js
@@ -2,6 +2,7 @@ function load_programacion_html()
 {
   let img_seleccionada = document.querySelectorAll("#prog_p1 > .column_1 > img")
   let inicio=true;
+  let datazo=0;
   if(inicio)
     img_seleccionada[0].style="opacity: 1;"
     for (let i = 0; i < img_seleccionada.length; i++)
@@ -13,6 +14,26 @@ function load_programacion_html()
           img_seleccionada[j].style="opacity: 0.5;"
         }
         img_seleccionada[i].style="opacity: 1;"
+        datazo=i;
       })
+
+      img_seleccionada[i].addEventListener("mouseover", () => {
+        for (let j = 0; j < img_seleccionada.length; j++) {
+          if(j==i) continue;
+          img_seleccionada[j].style="opacity: 0.5;"
+        }
+        img_seleccionada[i].style="opacity: 1;"
+      })
+
+
+      img_seleccionada[i].addEventListener("mouseout", () => {
+        for (let j = 0; j < img_seleccionada.length; j++) {
+          if(j==datazo) continue;
+          img_seleccionada[j].style="opacity: 0.5;"
+        }
+        img_seleccionada[datazo].style="opacity: 1;"
+      })
+
+
       }
   }
